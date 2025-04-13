3f225912cca482c32d9177bfa0d899b55e073e49
utsav <utsavzalavadiya21@gmail.com>
2025-04-13 12:34:05 +0530

    commit

---
+++ module 6/practical/FAB/app/src/main/java/com/example/fab/MainActivity.kt
@@ -0,0 +1,28 @@
+
+package com.example.fab
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+
+        fab.setOnClickListener {
+
+            Snackbar.make(it, "FAB Clicked!", Snackbar.LENGTH_SHORT).show()
+
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+    }
+}
